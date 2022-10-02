ccf21ea897848183101b853da5e2307707f20bab
16bithero <renzzi.adorador@gmail.com>
2022-10-02 00:27:33 -0400

    Updated Week 4 Files

--- Week 4/101277841_COMP3123-exec04/101277841_COMP3123-exec04.js
+++ Week 4/101277841_COMP3123-exec04/101277841_COMP3123-exec04.js
@@ -2,27 +2,43 @@ const express = require("express")
 const SERVER_PORT = 8088
 const app = express()
 
-app.get("/", (req,res) => {
+//Displayed when accessing the server
+app.get("/", (req, res) => {
     res.send("<h1>Lab 4 Exercise JS</h1>")
 })
 
+//GET request: /hello return "Hello Express JS"
 app.get("/hello", (req, res) => {
     res.send("<h1>Hello Express JS")
 })
 
+// //GET request: /user return that uses QueryString to send values
+//http://localhost:8088/user?firstname=Pritesh&lastname=Patel
 app.get("/user", (req, res) => {
-    var user = {
-        firstname: "Pritesh", 
-        lastname: "Patel"
+    const firstname = req.query.firstname
+    const lastname = req.query.lastname
+
+    res.send({
+        firstname,
+        lastname
+    })
+})
+
+//POST request: /user return that uses Path Parameter to send values
+//http://localhost:8088/user/Pritesh/Patel
+app.post("/user/:firstname/:lastname", (req, res) => {
+    //res.send(JSON.stringify(req.params))
+    const user = req.params
+    var firstname = user.firstname
+    var lastname = user.lastname
+
+    const newUser = {
+        firstname,
+        lastname
     }
-    res.send(JSON.stringify(user))
+    res.json(newUser)
 })
-
-app.post("/user/:firstname/:lastname", (req,res) => {
-    res.send(JSON.stringify(req.params))
-})
-
 
 app.listen(SERVER_PORT, () => {
-    console.log(`Server Started at http://localhost:${SERVER_PORT}`)  
+    console.log(`Server Started at http://localhost:${SERVER_PORT}`)
 })
